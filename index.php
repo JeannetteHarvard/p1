f88a9efd0e3da45cc519f7c161a4e09aef36cf28
Jeannette DuVal <duval@g.harvard.edu>
2016-09-15 15:53:43 -0400

    update

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<h1>P1 project</h1>
+<h1>Project 1</h1>
+
 <h2>by Jeannette DuVal</h2>
 
 <img src="images/jeannette.jpg" width=400>
+
+<h3>Project 2: Title</h3>
+View website: [link will be published here]
+<br><br>
+Github: [link will be published here]
+
+<h3>Project 3: Title</h3>
+View website: [link will be published here]
+<br><br>
+Github: [link will be published here]
+
+<h3>Project 4: Title</h3>
+View website: [link will be published here]
+<br><br>
+Github: [link will be published here]
